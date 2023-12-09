be01a357bc0babe1f9c03375cf6fe765aeb348b0
prateek041 <prateeksingh9741@gmail.com>
2023-12-10 00:59:34 +0530

    Created activity_create route
    Signed-off-by: prateek041 <prateeksingh9741@gmail.com>

--- flaskr/models/activity.py
+++ flaskr/models/activity.py
@@ -8,46 +8,14 @@ db = get_database()
 
 class Activity:
     def get_collection(self):
-        if db.get_collection(ACTIVITY) is not None:
+        if ACTIVITY in db.list_collection_names():
+            print(f"Found Collection {ACTIVITY}")
             return db.get_collection(ACTIVITY)
         else:
+            print(f"Not Found Collection {ACTIVITY}")
             return self.create_collection()
 
     # create_collection creates the "activity" collection in database.
     def create_collection(self):
         # Create the collection and return.
-        activity_collection = db.create_collection(
-            name="activity",
-            validator={
-                "bsonType": "object",
-                "title": "activity validation",
-                "required": ["steps", "active_calories_burned", "speed"],
-                "properties": {
-                    "steps": {
-                        "bsonType": "int",
-                        "description": "Number of steps up till now",
-                    },
-                    "active_calories_burned": {
-                        "bsonType": "object",
-                        "description": "Count of active calories burnt",
-                        "required": ["value", "unit"],
-                        "properties": {
-                            "value": {
-                                "bsonType": "int",
-                                "minimum": 0,
-                                "description": "Magnitude of Energy burn (Calories, Joules etc.)",
-                            },
-                            "unit": {
-                                "enum": ["Kcal", "KJoules"],
-                                "description": "Unit of Energy burnt",
-                            },
-                        },
-                    },
-                    "speed": {
-                        "bsonType": "int",
-                        "desciption": "Average speed while running",
-                    },
-                },
-            },
-        )
-        return db.get_collection(ACTIVITY)
+        return db.activity
--- flaskr/routes/activity.py
+++ flaskr/routes/activity.py
@@ -1,13 +1,32 @@
 """This file contains the blueprint and views for /user/health"""
 from flask import Blueprint, jsonify, request
 from flaskr.models.activity import Activity
+from bson import json_util
 
-user_activity_bp = Blueprint("auth", __name__, url_prefix="/user")
+user_activity_bp = Blueprint("user", __name__, url_prefix="/user")
+
+
+# get the Activity collection
+activity_collection = Activity().get_collection()
 
 
 @user_activity_bp.route("/activity", methods=("GET", "POST"))
-def get_user_activity():
+def create_user_activity():
     if request.method == "GET":
-        return jsonify("This is user activity")
-    else:
-        return jsonify("THis was post method")
+        return jsonify("This is the get route")
+    # if request.method == "GET":
+    #     result = activity_collection.find({})
+    #     document = list(result)[0]
+    #     returnjson.loads(json_util.dumps(document))
+    #     return jsonify("hello")
+    elif request.method == "POST":
+        test_activity = {
+            "steps": 2,
+            "active_calories_burned": {
+                "value": 10,
+                "unit": "Kcal",
+            },
+            "speed": 10,
+        }
+        post_id = activity_collection.insert_one(test_activity).inserted_id
+        return jsonify("This was post method")
